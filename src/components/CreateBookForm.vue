10726db666417cfcc8b83567305ece5f9c99d1ea
themis1986 <themis_1986@hotmail.com>
2022-08-19 11:11:28 +0300

    Add creating new document functionality

--- src/components/CreateBookForm.vue
+++ src/components/CreateBookForm.vue
@@ -14,6 +14,8 @@
 
 <script>
 import { ref } from "vue";
+import { db } from "../firebase/config";
+import { addDoc, collection } from "firebase/firestore";
 
 export default {
   setup() {
@@ -21,7 +23,15 @@ export default {
     const author = ref("");
 
     const handleSubmit = async () => {
-      console.log(title.value, author.value);
+      const colRef = collection(db, "books");
+      await addDoc(colRef, {
+        title: title.value,
+        author: author.value,
+        isFav: false,
+      });
+      //reset form
+      title.value = "";
+      author.value = "";
     };
 
     return { handleSubmit, title, author };
