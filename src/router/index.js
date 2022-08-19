88c99c975d63161d078a30c83a7b5ea051e06a84
themis1986 <themis_1986@hotmail.com>
2022-08-19 12:50:22 +0300

    Add navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -5,21 +5,42 @@ import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
 import Signup from "../views/Signup.vue";
 
+import { auth } from "../firebase/config";
+
+// route guard
+const requireAuth = (to, from, next) => {
+  if (auth.currentUser) {
+    next();
+  } else {
+    next({ name: "Login" });
+  }
+};
+const requireNoAuth = (to, from, next) => {
+  if (!auth.currentUser) {
+    next();
+  } else {
+    next({ name: "Home" });
+  }
+};
+
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+    beforeEnter: requireAuth,
   },
   {
     path: "/login",
     name: "Login",
     component: Login,
+    beforeEnter: requireNoAuth,
   },
   {
     path: "/signup",
     name: "Signup",
     component: Signup,
+    beforeEnter: requireNoAuth,
   },
 ];
 
