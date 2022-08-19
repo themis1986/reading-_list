ec683720b5bcecc2edbcb11bb306da36641d1ad3
themis1986 <themis_1986@hotmail.com>
2022-08-19 11:35:51 +0300

    Add updating documents functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,10 @@
           <h3 @click="handleDelete(book)">{{ book.title }}</h3>
           <p>By {{ book.author }}</p>
         </div>
-        <div class="icon">
+        <div
+          :class="{ icon: true, fav: book.isFav }"
+          @click="handleUpdate(book)"
+        >
           <span class="material-icons">favorite</span>
         </div>
       </li>
@@ -19,7 +22,7 @@
 import CreateBookForm from "@/components/CreateBookForm";
 import getCollection from "@/composables/getCollection";
 import { db } from "../firebase/config";
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 
 export default {
   name: "Home",
@@ -33,7 +36,14 @@ export default {
       deleteDoc(docRef);
     };
 
-    return { books, handleDelete };
+    const handleUpdate = (book) => {
+      const docRef = doc(db, "books", book.id);
+      updateDoc(docRef, {
+        isFav: !book.isFav,
+      });
+    };
+
+    return { books, handleDelete, handleUpdate };
   },
 };
 </script>
@@ -67,5 +77,8 @@ export default {
 .icon {
   color: #bbbbbb;
   cursor: pointer;
+}
+.icon.fav {
+  color: #f83f5e;
 }
 </style>
